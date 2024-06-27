85ef4cac511fe1ae4d9481609e380bd81f49adf8
siennawheeler <sienna_wheeler1@my.vcccd.edu>
2024-06-27 02:30:40 +0000

    yay

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ def main():
     Code Your Program here
     ########################################
     """
+    x = 0
+    while x < 1:
+        try: 
+            number = int(input())
+        except ValueError:
+            print('Input must be numeric')
+        else:
+            print(number)
+            x = x + 1
 
     ########################################
     # Do not delete the return statement
